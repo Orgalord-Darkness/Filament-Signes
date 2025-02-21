0d34744be7408ba09a1f058074cd6e4ce134b141
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-02-21 16:53:27 +0100

    Création des Widgets de Personnes Handicapees

---
+++ signes/app/Filament/Widgets/PhSignalementESSMSWidget.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\ChartWidget;
+use App\Models\Signalement ; 
+use App\Models\Secteur ; 
+
+class PhSignalementESSMSWidget extends ChartWidget
+{
+    protected static ?string $heading = 'Personnes Handicapees  - Nb de signalements par ESSMS';
+
+    protected function getData(): array
+    {
+        $etabs = Secteur::all();
+
+        $annee = $this->filters['annee'] ?? null;
+        $directionId = $this->filters['direction'] ?? null;
+
+        foreach ($etabs as $ligne) {
+            $query = Signalement::join('secteurs', 'signalements.secteur_id', '=', 'secteurs.id')
+            ->where('secteurs.id', $ligne->id)
+            ->where('secteurs.code', 'PH')
+            ->select('signalements.*')
+            ->get();
+
+            if (!empty($annee)) {
+                $query = $query->where('created_at', '>=', $annee.'-01-01')->where('created_at', '<',  $annee.'-12-31');
+            }
+
+            if (!empty($directionId)) {
+                $query = $query->where('direction_id', $directionId);
+            }
+            $datas[] = $query->count();
+            $colors[] = 'rgb('.rand(0, 255).','.rand(0, 255).','.rand(0, 255).')';
+            $libelles[] = $ligne->libelle;
+        }
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Nb Signalement',
+                    'data'  => $datas,
+                    // 'backgroundColor'=>$colors, 
+                    'backgroundColor'=>[
+                        "#FF0000",
+                        "#00FF00",
+                        "#0000FF",
+                        "#FFFF00",
+                        "#00FFFF",
+                        "#FF00FF",
+                        "#000000",
+                        "#FFFFFF",
+                        "#808080",
+                        "#FFA500"
+                    ],
+                    'borderColor'     => 'rgb(255, 255, 255)',
+                ],
+            ],
+            'labels' => $libelles,
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'pie';
+    }
+}
