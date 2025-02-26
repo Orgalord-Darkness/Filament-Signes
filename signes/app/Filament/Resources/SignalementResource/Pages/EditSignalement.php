0875c4326e24da8b623e0b8b867e3dcda3510c78
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-02-26 10:15:49 +0100

    Test de décryptage

--- signes/app/Filament/Resources/SignalementResource/Pages/EditSignalement.php
+++ signes/app/Filament/Resources/SignalementResource/Pages/EditSignalement.php
@@ -32,29 +32,30 @@ class EditSignalement extends EditRecord
         //     echo "Erreur de déchiffrement : " . $e->getMessage();
         // }
         //Décryptage des champs
-        // $data['nature1_autre'] = Crypt::decrypt($data['nature1_autre']) ;
-        // $data['nature2_autre'] = Crypt::decrypt($data['nature2_autre']) ;
-        $data['description'] = Crypt::decrypt($data['description']) ;
-        // $data['periode_eig_autre'] = Crypt::decrypt($data['periode_eig_autre']) ; 
-        // $data['consequence1_autre'] = Crypt::decrypt($data['consequence1_autre']) ;
-        // $data['consequence2_autre'] = Crypt::decrypt($data['consequence2_autre']) ;
-        // $data['consequence3_autre'] = Crypt::decrypt($data['consequence3_autre']) ;
-        // $data['secours_non'] = Crypt::decrypt($data['secours_non']) ;
-        // $data['secours_autre'] = Crypt::decrypt($data['secours_autre']) ;
-        // $data['mesure1'] = Crypt::decrypt($data['mesure1']) ;
-        // $data['mesure2'] = Crypt::decrypt($data['mesure2']) ;
-        // $data['mesure3'] = Crypt::decrypt($data['mesure3']) ;
-        // $data['information_non'] = Crypt::decrypt($data['information_non']) ;
-        // $data['information_autre'] = Crypt::decrypt($data['information_autre']) ;
-        // $data['disposition1_autre'] = Crypt::decrypt($data['disposition1_autre']) ;
-        // $data['disposition2_autre'] = Crypt::decrypt($data['disposition2_autre']) ;
-        // $data['disposition3_autre'] = Crypt::decrypt($data['disposition3_autre']) ;
-        // $data['disposition4_autre'] = Crypt::decrypt($data['disposition4_autre']) ;
-        // $data['media1_oui'] = Crypt::decrypt($data['media1_oui']) ;
-        // $data['media2_oui'] = Crypt::decrypt($data['media2_oui']) ;
-        // $data['media3_oui'] = Crypt::decrypt($data['media3_oui']) ;
-        // $data['analyse_groupe_autre'] = Crypt::decrypt($data['analyse_groupe_autre']) ;
-        // $data['commentaire'] = Crypt::decrypt($data['commentaire']) ;
+        $data['nature1_autre'] = Crypt::decrypt($data['nature1_autre']) ; //marche pas 
+        $data['nature2_autre'] = Crypt::decrypt($data['nature2_autre']) ; //marche pas
+        $data['description'] = Crypt::decrypt($data['description']) ; //marche 
+        $data['periode_eig_autre'] = Crypt::decrypt($data['periode_eig_autre']) ; //marche pas
+        $data['consequence1_autre'] = Crypt::decrypt($data['consequence1_autre']) ; //marche pas
+        $data['consequence2_autre'] = Crypt::decrypt($data['consequence2_autre']) ; //marche pas
+        $data['consequence3_autre'] = Crypt::decrypt($data['consequence3_autre']) ; //marche pas
+        $data['secours_non'] = Crypt::decrypt($data['secours_non']) ; //marche pas
+        $data['secours_autre'] = Crypt::decrypt($data['secours_autre']) ; //marche pas
+        $data['mesure1'] = Crypt::decrypt($data['mesure1']) ;
+        $data['mesure2'] = Crypt::decrypt($data['mesure2']) ;
+        $data['mesure3'] = Crypt::decrypt($data['mesure3']) ;
+        $data['information_non'] = Crypt::decrypt($data['information_non']) ; //marche pas
+        $data['information_autre'] = Crypt::decrypt($data['information_autre']) ; //marche pas
+        $data['disposition1_autre'] = Crypt::decrypt($data['disposition1_autre']) ; //marche pas
+        $data['disposition2_autre'] = Crypt::decrypt($data['disposition2_autre']) ; //marche pas
+        $data['disposition3_autre'] = Crypt::decrypt($data['disposition3_autre']) ; //marche pas
+        $data['disposition4_autre'] = Crypt::decrypt($data['disposition4_autre']) ; //marche pas
+        $data['evolution'] = Crypt::decrypt($data['evolution']) ;
+        $data['media1_oui'] = Crypt::decrypt($data['media1_oui']) ; //marche pas 
+        $data['media2_oui'] = Crypt::decrypt($data['media2_oui']) ; //marche pas 
+        $data['media3_oui'] = Crypt::decrypt($data['media3_oui']) ; //marche pas 
+        $data['analyse_groupe_autre'] = Crypt::decrypt($data['analyse_groupe_autre']) ; //marche pas 
+        $data['commentaire'] = Crypt::decrypt($data['commentaire']) ; //marche 
 
         return $data ; 
 
